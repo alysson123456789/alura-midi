0b286a907aafcf4c1317261c92e13d41ccc45940
alysson123456789 <128196427+alysson123456789@users.noreply.github.com>
2023-11-10 21:38:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,20 +7,33 @@ document.querySelector('#som_tecla_clap').play();
 }
 
 function tocaSom(idElementoAudio){
-document.querySelector(idElementoAudio).play();
+const elemento = document.querySelector(idElementoAudio);
+ if (elemento && elemento.localName === 'audio') {
+       elemento.play();
+ } else {
+   alert('bala ou bala ou um curativo');
+ }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador=0;
-while(contador <listaDeTeclas.length) {
+for(let contador=0; contador <listaDeTeclas.length; contador++) {
   const tecla= listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio =`#som_${instrumento}`;
-  tecla.onclick =function () {
+  tecla.onclick =function () { 
   tocaSom(idAudio);
 };
-  contador = contador + 1;
-  console.log(contador);
+
+  tecla.onkeydown =function (evento) {
+  if (evento.code === 'Enter' || evento.code === 'Space' ) {
+  tecla.classList.add('ativa');
+  }
+  console.log(evento.code);
+}
+  
+  tecla.onkeyup =function () {
+  tecla.classList.remove('ativa');
+}
+  
   console.log(instrumento);
-  console.log(idAudio);
 }
